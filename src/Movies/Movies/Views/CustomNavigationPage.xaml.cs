8b6c77b6913cbaea4fdab29364f06bcd5d4c94e6
Mohamed CHOUCHANE <mohamed.chouchane1990@gmail.com>
2017-09-28 23:57:59 +0200

    Add Movies.WPF

--- src/Movies/Movies/Views/CustomNavigationPage.xaml.cs
+++ src/Movies/Movies/Views/CustomNavigationPage.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using Xamarin.Forms;
 using Xamarin.Forms.PlatformConfiguration;
-using Xamarin.Forms.PlatformConfiguration.GTKSpecific;
 
 namespace Movies.Views
 {
@@ -14,8 +13,6 @@ namespace Movies.Views
         public CustomNavigationPage(Page root) : base(root)
         {
             InitializeComponent();
-
-            this.On<GTK>().SetBackButtonIcon("Images/movies-back.png");
         }
     }
 }
